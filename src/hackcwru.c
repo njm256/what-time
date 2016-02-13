59a0a8e9a3ef881f9c8857de56fe97ff6780a8e1
Nicholas Marino <nicholas@marino-debian>
2016-02-13 02:15:45 -0500

    can display static message

--- src/hackcwru.c
+++ src/hackcwru.c
@@ -6,7 +6,26 @@ static TextLayer *text_layer;
 static void main_window_load(Window *window) {
     Layer *window_layer = window_get_root_layer(window);
     GRect bounds = layer_get_bounds(window_layer);
+
+    text_layer = text_layer_create(
+        GRect((int) (.1 * bounds.size.w),
+              (int) (.18 * bounds.size.h),
+              (int) (.8 * bounds.size.w),
+              (int) (.7 * bounds.size.h)));
+
+    text_layer_set_background_color(text_layer, GColorWhite);
+    text_layer_set_text_color(text_layer, GColorBlack);
+    text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28));
+    text_layer_set_text(text_layer, "\nTime to get a watch :P");
+    text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+
+    layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
+
+static void main_window_unload(Window *window) {
+    text_layer_destroy(text_layer);
+}
+
 
 static void init() {
     window = window_create();
@@ -15,11 +34,7 @@ static void init() {
         .unload = main_window_unload
     });
 
-    window_stack_push(main_window, true);
-}
-
-static void main_window_unload(Window *window) {
-
+    window_stack_push(window, true);
 }
 
 static void deinit() {
